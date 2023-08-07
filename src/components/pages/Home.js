3f126f0db65ed0be452199878e81cde34cd87034
lina-i-tahir <lina.i.tahir@gmail.com>
2023-08-07 12:10:08 +0800

    feat: add animation to /home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import "./Home.css";
 import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -9,10 +10,33 @@ const Home = () => {
       <div className="home-background">
         <Link to="/climbsmain">
           <h1 className="home">
-            Where's your <br></br>climb today?
+            <motion.div
+              className="home-ani"
+              animate={{ y: 10, opacity: 100 }}
+              initial={{
+                y: -50,
+                opacity: 100,
+              }}
+              transition={{
+                duration: 0.8,
+              }}
+            >
+              Where's your <br></br>climb today?
+            </motion.div>
           </h1>
           <div className="arrowIcon">
-            <FontAwesomeIcon icon={faAnglesDown} size="2xl" />
+            <motion.div
+              className="arrow-ani"
+              animate={{ y: 10, opacity: 100 }}
+              initial={{ y: -50, opacity: 100 }}
+              transition={{
+                duration: 1,
+                type: "spring",
+                stiffness: 60,
+              }}
+            >
+              <FontAwesomeIcon icon={faAnglesDown} size="2xl" />
+            </motion.div>
           </div>
         </Link>
       </div>
